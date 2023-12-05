4200ed99a7f13a0c2ca2c01edeb77a4e0aaeb7e0
Stefan Luchs <stefan.luchs@if.de>
2023-12-05 09:00:09 +0100

    AOC 2023 remove input

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,13 +1,13 @@
 import me.luchs.aoc2023.DayFour
 
 fun main() {
-    val input = readInput("DayFour")
+    val input = readInput("input/DayFour")
     val day = DayFour(input.trimIndent())
     println("Part One: " + day.partOne())
     println("Part Two: " + day.partTwo())
 }
 
 fun readInput(day: String): String {
-    return object {}.javaClass.getResourceAsStream("/$day")?.bufferedReader()?.use { it.readText() }
+    return object {}.javaClass.getResourceAsStream("/input/$day")?.bufferedReader()?.use { it.readText() }
         ?: throw IllegalArgumentException("Can not read file $day")
 }
