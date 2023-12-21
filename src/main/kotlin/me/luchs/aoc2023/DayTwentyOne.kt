fca363dc2789f1313e8b6580c169837314669633
Stefan Luchs <stefan.luchs@if.de>
2023-12-21 13:15:57 +0100

    AOC 2023 day 21 part 2

--- src/main/kotlin/me/luchs/aoc2023/DayTwentyOne.kt
+++ src/main/kotlin/me/luchs/aoc2023/DayTwentyOne.kt
@@ -8,45 +8,33 @@ data class DayTwentyOne(val input: String) : Day<Long> {
 
     private val matrix = CharMatrix(input)
 
-    val steps = 26501365
-
     override fun partOne(): Long {
         return positionsAfter(steps = 64)
     }
 
     override fun partTwo(): Long {
 
-        val dimensions = matrix.dimensions
+        val steps = 26501365
 
-        val start = matrix['S'].first()
+        val size = matrix.dimensions.first // first == second == 131
 
-        var possibilities = mutableListOf(start)
-        val visited = mutableSetOf<Coordinate>()
+        val rest = steps % size
+        val times = steps / size
 
+        // 26501365 = (times = 202300) * (size = 131) + (rest = 65)
 
-        val rest = steps % dimensions.first
-        val times = 26501365L / dimensions.first
-
-        // 26501365 = 202300 * 131 + 65
-
-
-        val value65 = positionsAfter(65)
-        val value65_131 = positionsAfter(65 + 131)
-        val value65_262 = positionsAfter(65 + (131 * 2))
+        val value65 = positionsAfter(rest) // 65
+        val value65_131 = positionsAfter(rest + size) // 65 + 131
+        val value65_262 = positionsAfter(rest + (size * 2)) // 65 + 2 * 131
 
         // solve value65 + value131 x + value262 x^2 = 202300 for x to find the solution
 
-        fun f(x: Long): Long =
-            value65 * (x - 1) * (x - 2) / (0 - 1) / (0 - 2) +
-                    value65_131 * (x - 0) * (x - 2) / (1 - 0) / (1 - 2) +
-                    value65_262 * (x - 0) * (x - 1) / (2 - 0) / (2 - 1)
+        fun f(x: Int): Long = value65 * (x - 1) * (x - 2) / 2 +
+                value65_131 * x * (x - 2) / -1 +
+                value65_262 * x * (x - 1) / 2
 
-        val res = f(202300)
-
-        // 3986414806023743
         // 636350496972143
-
-        return 0L
+        return f(x = times)
     }
 
     fun positionsAfter(steps: Int): Long {
